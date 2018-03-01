19a9cdb5af55640fa0d6dd27e8e9e3532efab34d
omer.sheikh <omer.sheikh@afiniti.com>
2018-03-01 19:02:39 +0500

    First Commit

---
+++ src/components/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class='about'>
+    <h1>Hello {{their_name}},This is an about page</h1>
+    <p>Non dolor perferendis. Doloremque perferendis aut delectus dolor ipsum sunt. Enim harum aut et. Autem ex aut fugiat et et impedit non quod neque. Saepe quia qui.</p>
+  </div>
+</template>
+<script>
+  export default {
+  name: 'Skills',
+  data () {
+    return {
+      their_name:this.$route.params.name
+    }
+  }
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,17 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import VeeValidate from 'vee-validate'
+import router from './router'
+
+Vue.use(VeeValidate);
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  router,
+  el: '#app',
+  components: { App },
+  template: '<App/>'
+})
